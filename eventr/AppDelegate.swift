fbd24886d48cdb27bf9afdb46da7cb6a523d89f4
Sagar <joezaza@dhcp-10-201-137-16.tamulink.tamu.edu>
2016-03-23 10:36:14 -0500

    Initial commit

--- eventr/AppDelegate.swift
+++ eventr/AppDelegate.swift
@@ -12,11 +12,19 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        
         // Override point for customization after application launch.
         return true
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
     func applicationWillResignActive(application: UIApplication) {
---
+++ eventr/NSMutableArray+FB_step_6.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+FB_step_6.h
+//  eventr
+//
+//  Created by Sagar  on 3/22/16.
+//  Copyright © 2016 cessation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray (FB_step_6)
+
+@end
